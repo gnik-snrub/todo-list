a8ad592f11eca52858e7701c40a3922ab1887273
Josiah Morris <josiahbmorris@hotmail.com>
2021-02-12 21:42:50 +1100

    Added projectFactory function. Added test project and todos.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ const todoFactory = (name, description, dueDate, priority) => {
     return { name, description, dueDate, priority, notes, status }
 }
 
-const factoryTest = todoFactory("Eat the dishes", "Bite into a stack of ceramic", "1/2/2021", 3)
+const test1 = todoFactory("Eat the dishes", "Bite into a stack of ceramic", "1/2/2021", 3)
+const test2 = todoFactory("Eeeeeeeeeeeeee", "Bbbbbbbbbbbbbbbbbbbbbbbbbbbb", "1/2/2021", 3)
 
-console.table(factoryTest)
+const projectFactory = (name, description) => {
+    const todos = []
+    
+    const addTodo = (newTodo) => { todos.push(newTodo) }
+    const removeTodo = (index) => { todos.splice(index, 1) }
+
+    return { name, description, todos, addTodo, removeTodo }
+}
+
+const projectTest = projectFactory("Default", "This is a test project");
+
+projectTest.addTodo(test1)
+projectTest.addTodo(test2)
+
+console.table(projectTest.todos)
