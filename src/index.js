7d1a7a6c7c0f9bb44373449cce017b0e2d066801
Josiah Morris <josiahbmorris@hotmail.com>
2021-02-12 19:54:13 +1100

    Created todoFactory function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './style.css';
+import './style.css'
 
-console.log('test')
+const todoFactory = (name, description, dueDate, priority) => {
+    const notes = [] 
+    const status = false //true = complete; false = uncomplete
+
+    return { name, description, dueDate, priority, notes, status }
+}
+
+const factoryTest = todoFactory("Eat the dishes", "Bite into a stack of ceramic", "1/2/2021", 3)
+
+console.table(factoryTest)
